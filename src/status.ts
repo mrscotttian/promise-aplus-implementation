5edfc6621643d847ffafea270ec1bcae1c99d02e
scotttian <scotttian2014@outlook.com>
2020-10-11 19:02:06 +0800

    Init project

---
+++ src/status.ts
@@ -0,0 +1,9 @@
+const PENDING: string = 'Pending';
+const FUlFILLED: string = 'Fulfilled';
+const REJECTED: string = 'Rejected';
+
+enum STATUS {
+    PENDING = 'Pending',
+    FUlFILLED = 'Fulfilled',
+    REJECTED = 'Rejected',
+}
