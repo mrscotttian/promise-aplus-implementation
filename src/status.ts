786140f9a55266e96ec7e72ab68aa22743780c8b
scotttian <scotttian2014@outlook.com>
2020-10-18 18:02:59 +0800

    updae

--- src/status.ts
+++
@@ -1,9 +0,0 @@
-const PENDING: string = 'Pending';
-const FUlFILLED: string = 'Fulfilled';
-const REJECTED: string = 'Rejected';
-
-enum STATUS {
-    PENDING = 'Pending',
-    FUlFILLED = 'Fulfilled',
-    REJECTED = 'Rejected',
-}
